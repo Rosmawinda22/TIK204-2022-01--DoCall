f86b93b688ebd0073aa3ce01cc32d235110df262
Rosmawinda22 <101490307+Rosmawinda22@users.noreply.github.com>
2022-05-08 23:41:04 +0700

    Delete inc_footer.php

--- website-konsultasi-dokter/admin/inc_footer.php
+++
@@ -1,9 +0,0 @@
-<main>
-    <footer class="bg-light">
-        <div class="text center p-3" style="background:#CCCCCC">
-        Copyright &copy; 2022
-        </div>
-    </footer>
-</body>
-
-</html>
